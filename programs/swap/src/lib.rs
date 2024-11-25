f7921bb7ef04c012fff5475df841aad9c0888d05
Sigma2345 <95545310+Sigma2345@users.noreply.github.com>
2024-11-26 02:26:02 +0530

    initial commit

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,28 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("GzVGcbMLNLxzW4ybgDw6DC3dmP4cnzSVzAekUDrmHvLT");
+
+#[program]
+pub mod swap {
+
+    use super::*;
+
+    pub fn make_offer(ctx: Context<MakeOffer>, id: u64, token_a_offered_amount: u64, token_b_wanted_amount: u64) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)
+    }
+
+    pub fn take_offer(ctx: Context<TakeOffer>, id: u64) -> Result<()> {
+        instructions::take_offer::send_wanted_tokens_to_maker(&ctx)?;
+        instructions::take_offer::withdraw_and_close_vault(ctx) 
+    }
+}
